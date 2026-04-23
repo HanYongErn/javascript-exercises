13420f6fd3a7d7b94bf44f7ce4899f5f13546471
HanYongErn <HanYongErn@gamuda.com.my>
2026-04-23 11:17:24 +0800

    compete foundation_06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
-
-};
+const repeatString = function repeatString(string, number){
+    if (number > 0) {
+        /*const final = string.repeat(number);
+        return final;*/
+        let result = "";
+        let i = 0;
+        while (i<number) {
+            result += string;
+            i++;
+        }
+        return result;
+    } else if (number < 0) {
+        return "ERROR";
+    } else {
+        return "";
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;

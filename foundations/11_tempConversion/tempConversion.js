3afde3c53b4d473ffbedf600eff389ed9d08d677
HanYongErn <HanYongErn@gamuda.com.my>
2026-04-28 22:31:49 +0800

    complete foundation_11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+//(0°F − 32) × 5/9 = -17.78°C
+//(°F − 32) × 5/9 = °C
+const convertToCelsius = function convertToCelcius(F) {
+  return(Math.round(((F - 32) * 5 / 9) * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+//(0°C × 9/5) + 32 = 32°F
+//(°C × 9/5) + 32 = °F
+const convertToFahrenheit = function convertToFarenheit(C) {
+  return(Math.round(((C * 9 / 5) + 32) * 10) /10);
 };
 
 // Do not edit below this line

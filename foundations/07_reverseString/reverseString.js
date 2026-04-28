085d284d5383b5831a8dd12a9aec39f55af85557
HanYongErn <HanYongErn@gamuda.com.my>
2026-04-28 14:19:25 +0800

    complete foundation_07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function reverseString(str) {
+    const string = str.split('');
+    let result = [];
+    for(let i = string.length - 1; i >= 0; i--) {
+        result.push(string[i]);
+    }
+    return result.join('');
+}
 // Do not edit below this line
 module.exports = reverseString;

dd33b4bf5b753473dc4a871041114da0dcd6e4ba
HanYongErn <HanYongErn@gamuda.com.my>
2026-04-28 16:20:47 +0800

    complete foundation_08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function removeFromArray(arr, ...arg) {
+    let result = [];
+    arr.forEach((num) => {
+        if(arg.indexOf(num) == -1) {
+            result.push(num);
+        }
+    })
+    console.log(result);
+    return result;
 };
 
 // Do not edit below this line

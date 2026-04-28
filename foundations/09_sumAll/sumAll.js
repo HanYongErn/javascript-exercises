26aa0cabf053051f70863e409e2545c67253698f
HanYongErn <HanYongErn@gamuda.com.my>
2026-04-28 17:10:40 +0800

    complete foundation_09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function sumAll(a, b) {
+    //check non-integer or negative numbers
+    if (Number.isInteger(a) !== true || Number.isInteger(b) !== true || 
+        a < 0 || b < 0) {
+        return "ERROR";
+    } else if(a > b) {
+        // make sure a smaller than b
+        let change;
+        change = b;
+        b = a;
+        a = change;
+    }
 
+    let sum = 0;
+    for(let i = a; i <= b; i++) {
+        sum += i;
+    }
+    console.log(sum);
+    return sum;
 };
 
 // Do not edit below this line

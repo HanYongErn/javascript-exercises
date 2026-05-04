75fbce6512c0cbb7b5b2f6f19dedba98191fdc93
HanYongErn <HanYongErn@gamuda.com.my>
2026-05-04 14:10:05 +0800

    complete foundation_14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    let answer;
+    let a = 1
+    let b = 1
+    if (num < 0) {
+        return "OOPS";
+    } else if (num == 0) {
+        return 0;
+    } else {
+        for (let n = 1; n <= +num; n++) {
+            if (n <= 2) {
+                answer = a;
+            } else {
+                answer = a + b;
+                a = b;
+                b = answer;
+            }
+        }
+        return answer;
+    }
 };
 
 // Do not edit below this line

0c7e6b25fddb35cc9f0da2acbb628f52066cf621
HanYongErn <HanYongErn@gamuda.com.my>
2026-05-04 14:51:30 +0800

    complete foundation_16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const oldest = arr.sort(function(a, b) {
+        let ageA;
+        let ageB;
+        if ("yearOfDeath" in a == false) {
+            let today = new Date();
+            today = today.getFullYear();
+            ageA = today - a.yearOfBirth;
+        } else {
+            ageA = a.yearOfDeath - a.yearOfBirth;
+        }
+        if ("yearOfDeath" in b == false) {
+            let today = new Date();
+            today = today.getFullYear();
+            ageB = today - b.yearOfBirth;
+        } else {
+            ageB = b.yearOfDeath - b.yearOfBirth;
+        }
 
-};
+        return ageA > ageB ? -1 : 1;
+    })
+    return arr[0];
+    
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
